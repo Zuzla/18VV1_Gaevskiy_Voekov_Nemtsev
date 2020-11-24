1cf0cde94261dd9612cf800c1076f8dbdd123520
Zuzla <48437594+Zuzla@users.noreply.github.com>
2020-11-24 21:46:50 +0300

    Переделали работу 5
    сделали новую программу в пятой работе. Теперь работает корректно

--- save_internet_5/save_internet_5/Source.cpp
+++ save_internet_5/save_internet_5/Source.cpp
@@ -1,122 +1,241 @@
 #define _CRT_SECURE_NO_WARNINGS
-
-#include <stdio.h>
-#include <windows.h>
+#include <Windows.h>
 #include <AccCtrl.h>
 #include <Aclapi.h>
-#include <tchar.h>
-
-#ifdef _DEBUG
 #include <conio.h>
-#endif
+#include <stdio.h>
 
-void error(const char *error) {
-	printf("%s\n", error);
+//написали программу заново, предыдущая работала некоректно)))
 
-#ifdef _DEBUG
-	_getch();
-#endif
-	exit(1);
-}
-
-int main(int argc, char* argv[]) {
-
+int main()
+{
+	// Объявляем структуру EXPLICIT_ACCESS для двух групп
 	EXPLICIT_ACCESS ea[2];
+	// Предварительно обнуляем
 	ZeroMemory(&ea, 2 * sizeof(EXPLICIT_ACCESS));
 
-	//получение идентификатора группы Everyone
-
+	// Получение идентификатора группы Everyone
 	SID_IDENTIFIER_AUTHORITY SIDAuthWorld = SECURITY_WORLD_SID_AUTHORITY;
 	PSID pEveryoneSID = NULL;
 
-	if (!AllocateAndInitializeSid(&SIDAuthWorld, 1, SECURITY_WORLD_RID, 0, 0, 0, 0, 0, 0, 0, &pEveryoneSID)) {
-		error("AllocateAndInitializeSid Error");
+	if (!AllocateAndInitializeSid(
+		&SIDAuthWorld,
+		1,
+		SECURITY_WORLD_RID,
+		0, 0, 0, 0, 0, 0, 0,
+		&pEveryoneSID)) {
+		printf("AllocateAndInitializeSid Error");
 	}
 
-	ea[0].grfAccessPermissions = KEY_READ;
-	ea[0].grfAccessMode = SET_ACCESS;
+	// Заполнение структуры EXPLICIT_ACCESS для группы Everyone
+	ea[0].grfAccessPermissions = GENERIC_READ | GENERIC_EXECUTE; // Права на чтение и исполнение
+	ea[0].grfAccessMode = DENY_ACCESS;							 // Запретить 
 	ea[0].grfInheritance = NO_INHERITANCE;
 	ea[0].Trustee.TrusteeForm = TRUSTEE_IS_SID;
-	ea[0].Trustee.TrusteeType = TRUSTEE_IS_WELL_KNOWN_GROUP;
-	ea[0].Trustee.ptstrName = (LPTSTR)pEveryoneSID;
+	ea[0].Trustee.TrusteeType = TRUSTEE_IS_WELL_KNOWN_GROUP;	 // Для группы
+	ea[0].Trustee.ptstrName = (LPCH)pEveryoneSID;
 
-	//получение идентификатора группы Admin
-
-	SID_IDENTIFIER_AUTHORITY SIDAuthNT = SECURITY_NT_AUTHORITY;
+	//получение идентификатора группы Администраторы
+	SID_IDENTIFIER_AUTHORITY SIDAuthAdmin = SECURITY_NT_AUTHORITY;
 	PSID pAdminSID = NULL;
 
-	if (!AllocateAndInitializeSid(&SIDAuthNT, 2, SECURITY_BUILTIN_DOMAIN_RID, DOMAIN_ALIAS_RID_ADMINS, 0, 0, 0, 0, 0, 0, &pAdminSID)) {
-		FreeSid(pEveryoneSID);
-		error("AllocateAndInitializeSid Error");
+	if (!AllocateAndInitializeSid(&SIDAuthAdmin, 2,
+		SECURITY_BUILTIN_DOMAIN_RID,
+		DOMAIN_ALIAS_RID_ADMINS,
+		0, 0, 0, 0, 0, 0,
+		&pAdminSID)) {
+		printf("AllocateAndInitializeSid Error %u\n", GetLastError());
+		return FALSE;
 	}
 
-	ea[1].grfAccessPermissions = KEY_ALL_ACCESS;
-	ea[1].grfAccessMode = SET_ACCESS;
+	// Заполнение структуры EXPLICIT_ACCESS для группы Администраторы
+	ea[1].grfAccessPermissions = GENERIC_WRITE;	// Права на запись
+	ea[1].grfAccessMode = SET_ACCESS;			// Разрешить 
 	ea[1].grfInheritance = NO_INHERITANCE;
 	ea[1].Trustee.TrusteeForm = TRUSTEE_IS_SID;
-	ea[1].Trustee.TrusteeType = TRUSTEE_IS_GROUP;
-	ea[1].Trustee.ptstrName = (LPTSTR)pAdminSID;
+	ea[1].Trustee.TrusteeType = TRUSTEE_IS_WELL_KNOWN_GROUP; // Для группы
+	ea[1].Trustee.ptstrName = (LPCH)pAdminSID;
 
+
+	// Генерируется новый список ACL
 	PACL pACL = NULL;
 	DWORD dwRes = SetEntriesInAcl(2, ea, NULL, &pACL);
-
 	if (dwRes != ERROR_SUCCESS) {
 		FreeSid(pEveryoneSID);
 		FreeSid(pAdminSID);
-		error("SetEntriesInAcl Error");
+		printf("SetEntriesInAcl Error");
 	}
 
+	// Выделяем память под дескриптор безопасности
 	PSECURITY_DESCRIPTOR pSD = (PSECURITY_DESCRIPTOR)LocalAlloc(LPTR, SECURITY_DESCRIPTOR_MIN_LENGTH);
-
 	if (pSD == NULL) {
 		FreeSid(pEveryoneSID);
 		FreeSid(pAdminSID);
 		LocalFree(pACL);
-		error("LocalAlloc Error");
+		printf("LocalAlloc Error");
 	}
 
+
+	// Инициализируем дескриптор безопасности 
 	if (!InitializeSecurityDescriptor(pSD, SECURITY_DESCRIPTOR_REVISION)) {
 		FreeSid(pEveryoneSID);
 		FreeSid(pAdminSID);
-		LocalFree(pACL);
+		if (pACL)LocalFree(pACL);
 		LocalFree(pSD);
-		error("InitializeSecurityDescriptor Error");
+		printf("InitializeSecurityDescriptor Error");
 	}
-
 	if (!SetSecurityDescriptorDacl(pSD, TRUE, pACL, FALSE)) {
 		FreeSid(pEveryoneSID);
 		FreeSid(pAdminSID);
 		LocalFree(pACL);
 		LocalFree(pSD);
-		error("SetSecurityDescriptorDacl Error");
+		printf("SetSecurityDescriptorDacl Error");
 	}
 
+	// Теперь, когда дескриптор безопасности создан,
+	// его можно использовать при создание файла, заполнив структуру SECURITY_ATTRIBUTES
 	SECURITY_ATTRIBUTES sa;
 	sa.nLength = sizeof(SECURITY_ATTRIBUTES);
 	sa.lpSecurityDescriptor = pSD;
 	sa.bInheritHandle = FALSE;
 
-	HKEY hkSub;
-	DWORD dwDisposition;
-	LONG lRes = RegCreateKeyEx(HKEY_CURRENT_USER, _T("Software\\pi"), 0, NULL, 0, KEY_READ | KEY_WRITE, &sa, &hkSub, NULL);
+	HANDLE hFile = CreateFile("test.txt", GENERIC_ALL, NULL, &sa, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
 
-	printf("RegCreateKeyEx result: ");
-
-	switch (lRes) {
-
-	case ERROR_SUCCESS:
-		printf("OK!\n");
-		break;
-	default:
-		printf("ERROR!\n");
+	if (hFile) {
+		printf("Done! Check test.txt! ");
 	}
-	printf("\nFinished!");
+	else {
+		printf("Error! File not created!");
+	}
 
-#ifdef _DEBUG
-	_getch();
-#endif
-	return 0;
+	FreeSid(pEveryoneSID);
+	FreeSid(pAdminSID);
+	LocalFree(pACL);
+	LocalFree(pSD);
+
+	int gch = _getch();
 }
+
+
+
+
+//#include <stdio.h>
+//#include <windows.h>
+//#include <AccCtrl.h>
+//#include <Aclapi.h>
+//#include <tchar.h>
+//
+//#ifdef _DEBUG
+//#include <conio.h>
+//#endif
+//
+//void error(const char *error) {
+//	printf("%s\n", error);
+//
+//#ifdef _DEBUG
+//	_getch();
+//#endif
+//	exit(1);
+//}
+//
+//int main(int argc, char* argv[]) {
+//
+//	EXPLICIT_ACCESS ea[2];
+//	ZeroMemory(&ea, 2 * sizeof(EXPLICIT_ACCESS));
+//
+//	//получение идентификатора группы Everyone
+//
+//	SID_IDENTIFIER_AUTHORITY SIDAuthWorld = SECURITY_WORLD_SID_AUTHORITY;
+//	PSID pEveryoneSID = NULL;
+//
+//	if (!AllocateAndInitializeSid(&SIDAuthWorld, 1, SECURITY_WORLD_RID, 0, 0, 0, 0, 0, 0, 0, &pEveryoneSID)) {
+//		error("AllocateAndInitializeSid Error");
+//	}
+//
+//	ea[0].grfAccessPermissions = KEY_READ;
+//	ea[0].grfAccessMode = SET_ACCESS;
+//	ea[0].grfInheritance = NO_INHERITANCE;
+//	ea[0].Trustee.TrusteeForm = TRUSTEE_IS_SID;
+//	ea[0].Trustee.TrusteeType = TRUSTEE_IS_WELL_KNOWN_GROUP;
+//	ea[0].Trustee.ptstrName = (LPTSTR)pEveryoneSID;
+//
+//	//получение идентификатора группы Admin
+//
+//	SID_IDENTIFIER_AUTHORITY SIDAuthNT = SECURITY_NT_AUTHORITY;
+//	PSID pAdminSID = NULL;
+//
+//	if (!AllocateAndInitializeSid(&SIDAuthNT, 2, SECURITY_BUILTIN_DOMAIN_RID, DOMAIN_ALIAS_RID_ADMINS, 0, 0, 0, 0, 0, 0, &pAdminSID)) {
+//		FreeSid(pEveryoneSID);
+//		error("AllocateAndInitializeSid Error");
+//	}
+//
+//	ea[1].grfAccessPermissions = KEY_ALL_ACCESS;
+//	ea[1].grfAccessMode = SET_ACCESS;
+//	ea[1].grfInheritance = NO_INHERITANCE;
+//	ea[1].Trustee.TrusteeForm = TRUSTEE_IS_SID;
+//	ea[1].Trustee.TrusteeType = TRUSTEE_IS_GROUP;
+//	ea[1].Trustee.ptstrName = (LPTSTR)pAdminSID;
+//
+//	PACL pACL = NULL;
+//	DWORD dwRes = SetEntriesInAcl(2, ea, NULL, &pACL);
+//
+//	if (dwRes != ERROR_SUCCESS) {
+//		FreeSid(pEveryoneSID);
+//		FreeSid(pAdminSID);
+//		error("SetEntriesInAcl Error");
+//	}
+//
+//	PSECURITY_DESCRIPTOR pSD = (PSECURITY_DESCRIPTOR)LocalAlloc(LPTR, SECURITY_DESCRIPTOR_MIN_LENGTH);
+//
+//	if (pSD == NULL) {
+//		FreeSid(pEveryoneSID);
+//		FreeSid(pAdminSID);
+//		LocalFree(pACL);
+//		error("LocalAlloc Error");
+//	}
+//
+//	if (!InitializeSecurityDescriptor(pSD, SECURITY_DESCRIPTOR_REVISION)) {
+//		FreeSid(pEveryoneSID);
+//		FreeSid(pAdminSID);
+//		LocalFree(pACL);
+//		LocalFree(pSD);
+//		error("InitializeSecurityDescriptor Error");
+//	}
+//
+//	if (!SetSecurityDescriptorDacl(pSD, TRUE, pACL, FALSE)) {
+//		FreeSid(pEveryoneSID);
+//		FreeSid(pAdminSID);
+//		LocalFree(pACL);
+//		LocalFree(pSD);
+//		error("SetSecurityDescriptorDacl Error");
+//	}
+//
+//	SECURITY_ATTRIBUTES sa;
+//	sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+//	sa.lpSecurityDescriptor = pSD;
+//	sa.bInheritHandle = FALSE;
+//
+//	HKEY hkSub;
+//	DWORD dwDisposition;
+//	LONG lRes = RegCreateKeyEx(HKEY_CURRENT_USER, _T("Software\\pi"), 0, NULL, 0, KEY_READ | KEY_WRITE, &sa, &hkSub, NULL);
+//
+//	printf("RegCreateKeyEx result: ");
+//
+//	switch (lRes) {
+//
+//	case ERROR_SUCCESS:
+//		printf("OK!\n");
+//		break;
+//	default:
+//		printf("ERROR!\n");
+//	}
+//	printf("\nFinished!");
+//
+//#ifdef _DEBUG
+//	_getch();
+//#endif
+//	return 0;
+//}
 
 
 //#define _CRT_SECURE_NO_WARNINGS
